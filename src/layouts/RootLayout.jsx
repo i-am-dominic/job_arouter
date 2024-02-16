1c2720ae78d23fdb781b30585d124ee6b0340113
Dominic Iyasele <dominiciyasele@gmail.com>
2024-02-16 09:29:34 +0100

    Initial Commit

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -6,10 +6,12 @@ const RootLayout = () => {
       <header>
         <nav>
           <h1>Jobarouter</h1>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='about'>About</NavLink>
-          <NavLink to='help'>help</NavLink>
-          <NavLink to='careers'>Careers</NavLink>
+          <div className="nav-links">
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='about'>About</NavLink>
+            <NavLink to='help'>help</NavLink>
+            <NavLink to='careers'>Careers</NavLink>
+          </div>
         </nav>
       </header>
 
